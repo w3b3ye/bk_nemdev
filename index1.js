aa7cc4b19eedb389462c45adc16758a560251e67
w3b3ye@gmail.com <w3b3ye@gmail.com>
2021-06-27 13:41:27 +0400

    Minor changes.

--- index1.js
+++ index1.js
@@ -4,6 +4,8 @@ var path = require("path");
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+
 app.listen(3000, function () {
   console.log("App is listening on port 3000.");
 });
