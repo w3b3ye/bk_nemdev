01da841c4b113a9b95940cf1e92638023a6fb5d8
w3b3ye <w3b3ye@gmail.com>
2021-06-27 16:07:36 +0000

    Blog pages are up.

--- cblog/index.js
+++ cblog/index.js
@@ -9,3 +9,30 @@ app.use(express.static("public"));
 app.listen(4000, function () {
   console.log("App is listening on port 4000.");
 });
+
+
+app.get("/", function (req, res) {
+  //Sending home page.
+  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+});
+
+app.get("/contact", function (req, res) {
+  //Sending a html page for contact.
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+});
+
+app.get("/about", function (req, res) {
+  //Sending a html page for about.
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+});
+
+app.get("/post", function (req, res) {
+  //Sending a html page for about.
+  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+});
+
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.resolve(__dirname, "pages/nofound.html"));
+});
+
