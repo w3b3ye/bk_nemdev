88ed29014d7a80fd37159675e9ff5a4df77d73f9
w3b3ye <w3b3ye@gmail.com>
2021-07-07 15:38:52 +0000

    Minor changes.

--- cblog/controllers/home.js
+++ cblog/controllers/home.js
@@ -2,6 +2,7 @@ var blogpost = require("../models/blogpost.js");
 
 module.exports = async (req, res) => {
     var blogposts = await blogpost.find({});
+    //console.log(req.session);
     res.render("index", {
         blogposts: blogposts
     });
